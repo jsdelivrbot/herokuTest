2f1b731909ed9850686b32ebb1d2e5e7db51beb9
bastien girschig <bastien.girschig@gmail.com>
2017-07-14 18:32:12 +0200

    trying to add db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 let cool = require('cool-ascii-faces');
+let pg = require('pg');
+
+let pool = new pg.Pool()
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -23,6 +26,19 @@ app.get('/times', function(request, response){
 	let times = process.env.TIMES || 5;
 	for (var i = 0; i < times; i++) result += "hello: "+i+"\n";
 	response.send(result);
+})
+
+app.get('/db', function (request, response) {
+	pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('SELECT * FROM test_table', function(err, result) {
+			done();
+			if (err) {
+				console.error(err); response.send("Error " + err);
+			} else {
+				response.render('pages/db', {results: result.rows} );
+			}
+		});
+	});
 })
 
 app.listen(app.get('port'), function() {
