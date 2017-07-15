58a89c8a94872dfde93e7be63501c47c095a9565
bastien girschig <bastien.girschig@gmail.com>
2017-07-15 15:10:05 +0200

    db working locally

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var app = express();
 let cool = require('cool-ascii-faces');
 let pg = require('pg');
 
+const config = {
+  user: "ekxblrinsypdus",
+  database: "dauq65784356dt",
+  password: "c1c7918c003da72747579b873b09d74fd8b873a6080d384af0a8fbf6799a6525",
+  host: "ec2-54-163-254-143.compute-1.amazonaws.com",
+  port: "5432",
+  max: 10,
+  idleTimeoutMillis: 30000,
+  ssl: true
+};
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -26,17 +37,24 @@ app.get('/times', function(request, response){
 	response.send(result);
 })
 
+let pool = new pg.Pool(config)
 app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err){
-				console.error(err); response.send("Error " + err);
-			} else {
-				response.render('pages/db', {results: result.rows} );
-			}
-    });
-  });
+	console.log("=>"+process.env.DATABASE_URL);
+	pool.connect(function(err, client, done) {
+		if(err){
+			console.log(err)
+		}else{
+			client.query('SELECT * FROM test_table', function(err, result) {
+				if (err){
+					console.error(err);
+					response.send("Error " + err);
+				} else {
+					response.render('pages/db', {results: result.rows} );
+				}
+    		});
+		}
+		done();
+	});
 })
 
 app.listen(app.get('port'), function() {
