69497a3b993110b7e245b9c0e6fd545844556700
bastien girschig <bastien.girschig@gmail.com>
2017-07-16 18:33:57 +0200

    demo app <=> server connection

--- src/main.js
+++ src/main.js
@@ -7,4 +7,10 @@ var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 function getGreeting(){
 	return "hello";
 }
+
+console.log("fetch")
+fetch("https://supermarche-monsieur-10270.herokuapp.com/test", {mode: 'cors'})
+.then(r=>r.text())
+.then(text=>alert(text))
+
 module.exports.getGreeting = getGreeting;
