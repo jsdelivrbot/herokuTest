cd6528200303b7bc2a2bdb8831ec875d57cc2e4c
bastien girschig <bastien.girschig@gmail.com>
2017-07-16 16:56:06 +0200

    stuff

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 var unique = require('uniq');
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
-console.log(unique(data));
 
 function getGreeting(){
-	return "hello2";
+	return "hello";
 }
 module.exports.getGreeting = getGreeting;
