855732453621b0a95f2ac6b7118eddc8d9567716
bastien girschig <bastien.girschig@gmail.com>
2017-07-16 18:33:16 +0200

    fix tests when there is a "fetch" in the script

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// Support server-side fetch for tests.
+let fetch = (typeof window === 'undefined') ? require('node-fetch') : window.fetch;
+
 var unique = require('uniq');
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
