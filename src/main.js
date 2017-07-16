87235f50bddde8c91aafad02ee58649e53c39b43
bastien girschig <bastien.girschig@gmail.com>
2017-07-16 19:39:39 +0200

    improved gulp build, (notifications, settings, watch) and devEnv script (local server + gulp watch)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-// Support server-side fetch for tests.
-let fetch = (typeof window === 'undefined') ? require('node-fetch') : window.fetch;
-
 var unique = require('uniq');
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
@@ -8,9 +5,11 @@ function getGreeting(){
 	return "hello";
 }
 
-console.log("fetch")
-fetch("https://supermarche-monsieur-10270.herokuapp.com/test", {mode: 'cors'})
-.then(r=>r.text())
-.then(text=>alert(text))
+if (typeof window !== 'undefined') {
+	console.log("fetch")
+	fetch("https://supermarche-monsieur-10270.herokuapp.com/test", {mode: 'cors'})
+	.then(r=>r.text())
+	.then(text=>alert(text))
+}
 
 module.exports.getGreeting = getGreeting;
